f2e88ef9c9bb18efec8cd26947ed0c5680bb9d5e
davikim1029 <46108691+davikim1029@users.noreply.github.com>
2025-09-05 00:16:43 -0500

    Update scanner.py start time for reload testing

--- services/scanner/scanner.py
+++ services/scanner/scanner.py
@@ -177,8 +177,8 @@ def run_scan(mode=None, consumer=None, debug=False):
             "services/scanner/buy_loop.py",
             "services/scanner/buy_scanner.py"
         ],
-        start_time=time(9, 30),   # 9:30 AM
-        end_time=time(23, 59),     # 4:00 PM
+        start_time=time(0, 00),   # 9:30 AM
+        end_time=time(17, 00),     # 4:00 PM
         cooldown_seconds=300      # wait 5 minutes before restarting
     )
 
